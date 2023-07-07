17f3c13305ff0eb82d8350e7995949c171e36a1f
ValentinShilin <valentin.shilin.de@gmail.com>
2023-07-07 17:46:30 +0200

    first

--- src/main/scala/akka_akka_streams/Akka/akka.scala
+++ src/main/scala/akka_akka_streams/Akka/akka.scala
@@ -8,38 +8,3 @@ import akka.actor.typed.ActorRef
 import akka.actor.typed.SpawnProtocol
 import akka.actor.typed.{PostStop}
 
-
-object intro_actors {
-  //1 functional
-  object behaviour_factory_methods{
-    object Echo {
-      def apply(): Behavior[String] = Behaviors.setup{ ctx =>
-        Behaviors.receiveMessage{
-          case msg =>
-            ctx.log.info(msg)
-            Behaviors.same
-        }
-      }
-    }
-  }
-
-  //2 OOP
-  object abtsract_behaviour{
-    object Echo {
-      def apply(): Behavior[String] = Behaviors.setup{ ctx=>
-        new Echo(ctx)
-      }
-    }
-
-    class Echo(ctx: ActorContext[String]) extends AbstractBehavior[String](ctx){
-      def onMessage(msg: String): Behavior[String] ={
-        ctx.log.info(msg)
-        this
-      }
-    }
-
-  }
-
-
-
-}
--- src/main/scala/akka_akka_streams/alpakka/ConsumerApp.scala
+++ src/main/scala/akka_akka_streams/alpakka/ConsumerApp.scala
@@ -15,29 +15,3 @@ import ch.qos.logback.classic.{Level, Logger}
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.slf4j.LoggerFactory
 
-object  ConsumerApp extends App{
-  implicit val system: ActorSystem = ActorSystem("consumer-sys")
-  implicit val mat: Materializer  = ActorMaterializer()
-  implicit val ec: ExecutionContextExecutor = system.dispatcher
-  LoggerFactory
-    .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
-    .asInstanceOf[Logger]
-    .setLevel(Level.ERROR)
-
-  val config = ConfigFactory.load()
-  val consumerConfig = config.getConfig("akka.kafka.consumer")
-  val consumerSettings = ConsumerSettings(consumerConfig, new StringDeserializer, new StringDeserializer)
-
-  val consume = Consumer
-    .plainSource(consumerSettings, Subscriptions.topics("test"))
-    .runWith(Sink.foreach(println))
-
-
-  consume onComplete {
-    case Success(_) => println("Done"); system.terminate()
-    case Failure(err) => println(err.toString); system.terminate()
-
-  }
-
-
-}
