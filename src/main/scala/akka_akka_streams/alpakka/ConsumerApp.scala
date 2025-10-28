cc5679bf85a979ce1475d72156638d7cdadbd950
ValentinShilinDe <103489688+ValentinShilinDe@users.noreply.github.com>
2025-10-28 17:36:11 +0100

    Refactor ConsumerApp package and imports

--- src/main/scala/akka_akka_streams/alpakka/ConsumerApp.scala
+++ src/main/scala/akka_akka_streams/alpakka/ConsumerApp.scala
@@ -1,17 +1,36 @@
-package akka_akka_streams.alpakka
+package alpakka
 
-import akka.Done
 import akka.actor.ActorSystem
-import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
-import akka.kafka.scaladsl.{Consumer, Producer}
-import akka.stream.scaladsl.{Sink, Source}
+
+import scala.util.{Failure, Success}
+import akka.kafka.{ConsumerSettings, Subscriptions}
+import akka.stream.scaladsl.{Sink}
 import akka.stream.{ActorMaterializer, Materializer}
 import com.typesafe.config.ConfigFactory
-import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
-
-import scala.concurrent.{ExecutionContextExecutor, Future}
-import scala.util.{Failure, Success}
-import ch.qos.logback.classic.{Level, Logger}
-import org.apache.kafka.clients.producer.ProducerRecord
+import akka.kafka.scaladsl.Consumer
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.slf4j.LoggerFactory
+import ch.qos.logback.classic.{Level, Logger}
 
+object  ConsumerApp extends App {
+  implicit val system: ActorSystem = ActorSystem("producer")
+  implicit val mat: Materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
+
+  LoggerFactory
+    .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+    .asInstanceOf[Logger]
+    .setLevel(Level.ERROR)
+  val config = ConfigFactory.load()
+  val consumerConfig = config.getConfig("akka.kafka.consumer")
+
+  val consumerSettings = ConsumerSettings(consumerConfig, new StringDeserializer, new StringDeserializer)
+  val consumer = Consumer.plainSource(consumerSettings, Subscriptions.topics("test"))
+    .runWith(Sink.foreach(println))
+  consumer onComplete {
+    case Success(_) => println("Done"); system.terminate()
+    case Failure(err) => println(err.toString); system.terminate()
+  }
+
+
+}
